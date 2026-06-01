1e74f8001d6fa703d6d7381006d4e887d26e795e
Cetheres Alexis <cetherea@student.21-school.ru>
2026-06-01 17:36:26 +0300

    add test

--- src/s21_string.h
+++ src/s21_string.h
@@ -4,3 +4,4 @@ typedef unsigned long s21_size_t;
 #define S21_NULL ((void *)0) 
 
 const void *s21_memchr(const void *memptr, int ch, s21_size_t num); 
+char *s21_strncpy(char *dest, const char *src, s21_size_t n);
