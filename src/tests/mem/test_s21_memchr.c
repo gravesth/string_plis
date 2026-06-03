6748311397b2ebb53dab1ef6054fc8daf3ef5417
MaxonPy <cheezuschrist1337@mail.ru>
2026-06-03 04:05:33 +0300

    sync

--- src/tests/mem/test_s21_memchr.c
+++ src/tests/mem/test_s21_memchr.c
@@ -14,12 +14,12 @@ START_TEST(test_memchr_basic) {
 END_TEST
 
 START_TEST(test_memchr_array) { 
-  char *mass = {"123123", "dsfasdfasd", "dddsasd"}; 
-  char ch = '1';
+  char mass[] = {0x12, 0x34, 0x123, 0x2b}; 
+  unsigned ch = 0x123;
 
-  const int *orig = memchr(mass, ch, strlen(mass));
-  const int *s21_result = s21_memchr(mass, ch, strlen(mass));
-
+  const int *orig = memchr(mass, ch, sizeof(mass));
+  const int *s21_result = s21_memchr(mass, ch, sizeof(mass));
+  
   ck_assert_ptr_eq(orig, s21_result);
 
 }
