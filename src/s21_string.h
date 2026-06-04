f87a23362cafd697993fd7a829cf4a9ec3eba601
MaxonPy <cheezuschrist1337@mail.ru>
2026-06-04 08:13:38 +0300

    memcmp

--- src/s21_string.h
+++ src/s21_string.h
@@ -4,3 +4,4 @@ typedef unsigned long s21_size_t;
 #define S21_NULL ((void *)0) 
 
 const void *s21_memchr(const void *memptr, int ch, s21_size_t num); 
+const int *s21_memcmp(const void *str1, const void *str2, s21_size_t n);
