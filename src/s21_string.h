993c5db087778f52a61be5ad26fe20a17f54767b
Maxim Kiyaev <92125820+gravesth@users.noreply.github.com>
2026-06-05 11:23:04 +0300

    implemented the memcmp

--- src/s21_string.h
+++ src/s21_string.h
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <check.h>
+#include <string.h>
 
 typedef unsigned long s21_size_t;
 #define S21_NULL ((void *)0) 
 
-const void *s21_memchr(const void *memptr, int ch, s21_size_t num); 
+void *s21_memchr(const void *memptr, int ch, s21_size_t num); 
+int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
