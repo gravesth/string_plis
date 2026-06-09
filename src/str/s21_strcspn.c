8df3b0770ccfc2332ce082f76614eede7099ca45
Cetheres Alexis <cetherea@student.21-school.ru>
2026-06-09 19:17:55 +0300

    add test for strcspn

--- src/str/s21_strcspn.c
+++ src/str/s21_strcspn.c
@@ -1,6 +1,6 @@
 #include "../s21_string.h"
 
-s21_size_t strcspn(const char *str1, const char *str2) {
+s21_size_t s21_strcspn(const char *str1, const char *str2) {
   s21_size_t len = 0;
   int i = 0;
   int f = 1;
--- src/tests/test_runner.c
+++ src/tests/test_runner.c
@@ -5,6 +5,7 @@ int main() {
   srunner_add_suite(sr, memcmp_suite());
   srunner_add_suite(sr, memchr_suite());
   srunner_add_suite(sr, strncpy_suite());
+  srunner_add_suite(sr, strcspn_suite());
   srunner_run_all(sr, CK_NORMAL);
   int failed = srunner_ntests_failed(sr);
   srunner_free(sr);
