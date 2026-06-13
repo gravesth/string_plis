e96cdb6ab7092eba1b1ae718e64fcf9895d7f03a
Cetheres Alexis <cetherea@student.21-school.ru>
2026-06-13 15:01:53 +0300

    add test and makefile

--- src/str/s21_strerror.c
+++ src/str/s21_strerror.c
@@ -1,7 +1,7 @@
 #include "../string.h"
 #include "strerror.h"
 
-char* strerror(int errnum) {
+char* s21_strerror(int errnum) {
   static const char* error[] = CURRENT_SYSTEM_ERRORS;
   const char* message = NULL;
   if (errnum < 0 || errnum > MAX_ERROR_CODE || error[errnum] == NULL) {
