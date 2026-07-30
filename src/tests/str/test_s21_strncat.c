c2557bc5c67385da6216a067ba8b248419992fb7
XayXay-jpg <gamesmarat91@gmail.com>
2026-07-30 16:47:50 +0300

    all_two_part

--- src/tests/str/test_s21_strncat.c
+++ src/tests/str/test_s21_strncat.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+
 #include "../../s21_string.h"
 #include "../test_runner.h"
 
@@ -74,11 +75,26 @@ START_TEST(test_strncat_empty_src) {
 }
 END_TEST
 
+START_TEST(test_strncat_zero_n) {
+  const char *src = "world";
+  s21_size_t n = 0;
+  char *s21_dest = malloc(20 * sizeof(char));
+  char *dest = malloc(20 * sizeof(char));
+  strcpy(s21_dest, "hello");
+  strcpy(dest, "hello");
+  s21_strncat(s21_dest, src, n);
+  strncat(dest, src, n);
+  ck_assert_str_eq(s21_dest, dest);
+  free(s21_dest);
+  free(dest);
+}
+END_TEST
+
 Suite *strncat_suite(void) {
   Suite *s = suite_create("test block 1");
   TCase *tc_basic = tcase_create("Basic Tests");
-  tcase_add_test(tc_basic, test_strncat_basic); // 
-  tcase_add_test(tc_basic, test_strncat_partial_n); // 
+  tcase_add_test(tc_basic, test_strncat_basic);      //
+  tcase_add_test(tc_basic, test_strncat_partial_n);  //
   tcase_add_test(tc_basic, test_strncat_max_n);
   suite_add_tcase(s, tc_basic);
 
--- src/tests/test_runner.c
+++ src/tests/test_runner.c
@@ -12,8 +12,11 @@ int main() {
   srunner_add_suite(sr, strncat_suite());
   srunner_add_suite(sr, strerror_suite());
   srunner_add_suite(sr, strpbrk_suite());
+  srunner_add_suite(sr, sprintf_suite());
   srunner_run_all(sr, CK_NORMAL);
   int failed = srunner_ntests_failed(sr);
   srunner_free(sr);
   return (failed == 0) ? 0 : 1;
 }
+
+#include "str/test_s21_sprintf.c"
